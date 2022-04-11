cd4e51691234786fd5acb06dab63d338a37e3ae4
RicardoMendesAbadia <100205052+RicardoMendesAbadia@users.noreply.github.com>
2022-04-11 11:54:33 -0300

    Update torto.c

--- torto.c
+++ torto.c
@@ -1,19 +1,30 @@
 #include <stdio.h> 
 #include <ncurses.h> 
 #include <string.h>
+#include <ctype.h>
 
-char tabuleiro [6][4] = {{"rsi"}, {"epn"}, {"ega"}, {"idj"}, {"aoa"}, {"dir"}}; 
+char tabuleiro [6][4] = {{"rsi"}, {"epn"}, {"ega"}, {"idj"}, {"aoa"}, {"dir"}};
+typedef struct _cordenadas{
+    int x;
+    int y;
+}coordenada;
+coordenada posicao[18]; 
 
 void print_t(char t[6][4], int r, int c) {
-   for(int i = 0; i < 6; i++) {
-       for(int j = 0; j < 3; j++) {
-           mvprintw((r/2) - 5 + i * 2, (c/4) - 5 + 4 * j, "(");
-           attron(A_UNDERLINE | A_BOLD); 
-           printw("%c", toupper(t[i][j])); 
-           attroff(A_UNDERLINE | A_BOLD); 
-           printw(")"); 
-           if(j != 2) printw("-"); 
-       }
+    coordenada posletra;
+    int indice = 0;
+    for(int i = 0; i < 6; i++) {
+        for(int j = 0; j < 3; j++) {
+            mvprintw((r/2) - 5 + i * 2, (c/4) - 5 + 4 * j, "(");
+            attron(A_UNDERLINE | A_BOLD); 
+            printw("%c", toupper(t[i][j]));
+            posletra.y = (r/2) - 5 + i * 2;
+            posletra.x = (c/4) - 4 + 4 * j;
+            posicao[indice++] = posletra;
+            attroff(A_UNDERLINE | A_BOLD); 
+            printw(")"); 
+            if(j != 2) printw("-"); 
+        }
        
        if(i != 5) {
            mvprintw((r/2) - 4 + i * 2, c/4 - 4, "| X | X |");
@@ -49,6 +60,10 @@ int main() {
 
     print_t(tabuleiro, row, col);
     print_fields(row, col);
+
+    for(int x = 0; x < 18; x++){
+        printf("y = %d x = %d\n", posicao[x].y, posicao[x].x);
+    }
 
     getch();
 
